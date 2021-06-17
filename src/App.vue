e86b90d1d8f1a31c19d8d091f26293ae5932cdde
Yomna-Raouf <yomnaraouf99@gmail.com>
2021-06-17 16:41:44 +0200

    exploring data bindings, methods, filters, computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="display-4 text-center">Data Bindings</h1>
+          <h4 class="bg-primary text-white p-3">Fruit: {{ fruit }}</h4>
+          <h4 class="bg-dark text-white p-3">
+            Product: {{ product }} ${{ formattedPrice }}
+          </h4>
+          <h4 class="bg-secondary text-white p-3">
+            Product price with tax: {{ product }} ${{
+              getProductPriceWithTax(2)
+            }}
+          </h4>
+          <h4 class="bg-secondary text-white p-3">
+            Product price with tax: {{ product }} ${{
+              getProductPriceWithTax(5)
+            }}
+          </h4>
+          <h4 class="bg-info text-white p-3">
+            Product: {{ product }} {{ productPrice | currencyFilter }}
+          </h4>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      fruit: "apples",
+      product: "pen",
+      productPrice: 4
+    };
+  },
+  computed: {
+    formattedPrice() {
+      return (this.productPrice + 1).toFixed(2);
+    }
+  },
+  methods: {
+    getProductPriceWithTax(tax) {
+      this.productPrice = 10;
+      let taxValue = this.productPrice * (tax / 100);
+      return (this.productPrice + taxValue).toFixed(2);
+    }
+  },
+  filters: {
+    currencyFilter(value) {
+      return new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR"
+      }).format(value);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
